db68df3f347984d4db3814b700205bce924d5b1a
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-08-11 11:17:25 +0600

    statistics section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // import Image from 'next/image'
 // "use client"
 import HomeBanner from '@/components/Home/HomeBanner';
-import Statistics from '@/components/Home/Statistics';
 import Testimonial from '@/components/Home/Testimonial';
 import Newsletter from '@/components/Home/Newsletter';
 import TuitionTypes from '@/components/Home/TuitionTypes';
+import Statistics from '@/components/Home/Statistics';
 // import AOS from 'aos';
 
 export default function Home() {
