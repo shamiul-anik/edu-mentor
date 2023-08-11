e76d75b830e1dad43b6d522211967db46b64ec21
shamiul <anik.savar.bd@gmail.com>
2023-08-11 12:19:11 +0600

    Update from shamim and rezwan branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,8 @@ import HomeBanner from '@/components/Home/HomeBanner';
 import Testimonial from '@/components/Home/Testimonial';
 import Newsletter from '@/components/Home/Newsletter';
 import TuitionTypes from '@/components/Home/TuitionTypes';
-<<<<<<< HEAD
 import SuccessStory from '@/components/Home/SuccessStory';
-=======
 import Statistics from '@/components/Home/Statistics';
->>>>>>> db68df3f347984d4db3814b700205bce924d5b1a
 // import AOS from 'aos';
 
 export default function Home() {
