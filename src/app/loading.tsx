42e1a5dcc587f5c9c129fe8e866ce383844559c4
shamiul <anik.savar.bd@gmail.com>
2023-08-17 00:22:22 +0600

    Loader added for different routes

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+// "use client"
+import React from 'react';
+import Loader from '@/components/(shared)/Loader/Loader';
+
+const loading = () => {
+  return (
+    <Loader></Loader>
+  );
+};
+
+export default loading;
---
+++ src/components/(shared)/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Lottie from "lottie-react";
+import Loading from '@/assets/lottie/loading.json';
+
+const Loader = () => {
+  return (
+    <Lottie className="max-w-4xl mx-auto" animationData={Loading} loop={true} />
+  );
+};
+
+export default Loader;
