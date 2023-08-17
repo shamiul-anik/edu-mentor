8c47556eae75bac7840955544f7db6f7f621f8c5
Asadullah850 <mhshamim850@gmail.com>
2023-08-17 11:24:19 +0600

    17/8 befor start

---
+++ src/components/(shared)/Counter/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
+
+const Counter = ({ number }) => {
+	const [counterOn, setCounterOn] = useState(false);
+
+	const handleEnter = () => {
+		setCounterOn(true);
+	};
+
+	const handleExit = () => {
+		setCounterOn(false);
+	};
+
+	return (
+		<div>
+			<ScrollTrigger onEnter={handleEnter} onExit={handleExit}>
+				<div>
+					{counterOn && (
+						<CountUp start={0} decimals={Number.isInteger(number) ? 0 : 1} end={number} duration={2} delay={0} />
+					)}
+				</div>
+			</ScrollTrigger>
+		</div>
+	);
+};
+
+export default Counter;
