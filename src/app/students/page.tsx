4fe1e980f9c05f154c708a653f35e7cfb3616574
shamiul <anik.savar.bd@gmail.com>
2023-08-18 10:00:11 +0600

    Project design structure updated

---
+++ src/app/students/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Students = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+export default Students;
--- src/components/(shared)/Header/Header.tsx
+++ src/components/(shared)/Header/Header.tsx
@@ -17,6 +17,27 @@ const Header = () => {
 	const currentUserPhotoURL = UserImage;
 	const userRole = "student";
 
+
+	// const { user, userRole, logOut, loading, setLoading } = useContext(AuthContext);
+
+	// const navigate = useNavigate();
+
+	// const currentUserName = user?.displayName || "Welcome, User!";
+	// const currentUserPhotoURL = user?.photoURL || UserImage;
+	// const currentUserEmail = user?.email;
+
+	// const handleLogOut = () => {
+	// 	logOut()
+	// 		.then(() => {
+	// 			toast.success("Successfully logged out!");
+	// 			navigate("/");
+	// 		})
+	// 		.catch((error) => {
+	// 			console.log(error.message);
+	// 			setLoading(false);
+	// 		})
+	// };
+
 	return (
 		<div className="bg-teal-700 py-2">
 			<nav className={`navbar gap-4 ${user ? 'justify-between' : ''} max-w-7xl mx-auto`}>
@@ -31,7 +52,7 @@ const Header = () => {
 								<Link href="/">Home</Link>
 							</li>
 							<li className="hover:cursor-pointer">
-								<Link href="/teachers">Teachers</Link>
+								<Link href="/tutors">Tutors</Link>
 							</li>
 							<li className="hover:cursor-pointer">
 								<Link href="/students">Students</Link>
