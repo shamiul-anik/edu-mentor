b9f94aa79166af4fe256c29c5adb26f5f45e2461
saidur-way <saiduronway@gmail.com>
2023-08-22 20:53:54 +0600

    params type problem solve

--- src/app/[popularId]/page.tsx
+++ src/app/[popularId]/page.tsx
@@ -1,10 +1,11 @@
 
+import { Tutor } from "@/scriptType/tutorType";
 import getSingleTutor from "@/utils/getSingleTutor"
 import React from 'react';
 
-const singlePage = async ({params: {popularId}}) => {
-    const singleTutor= await getSingleTutor(popularId)
-    console.log(singleTutor)
+const singlePage: React.FC<{ params: { popularId: string | number } }> = async ({params: {popularId}}) => {
+    const singleTutor: Tutor= await getSingleTutor(popularId)
+    // console.log(singleTutor)
     return (
         <div>
             <h1>singlePage {popularId}</h1>
