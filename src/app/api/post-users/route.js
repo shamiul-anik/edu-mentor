9fe3067cb78187f9f6f5cab6282241ff52802975
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-08-25 22:12:51 +0600

    tutors page and students page added

--- src/app/api/post-users/route.js
+++ src/app/api/post-users/route.js
@@ -5,28 +5,28 @@ import { Users } from "../../../models/Users.js";
 export const POST = async (request) => {
   try {
     // Connect to the MongoDB database
+    const { userInfo } = await request.json();
     await connect();
 
-    const { userInfo } = request.body;
 
-    if (!userInfo) {
-      return new NextResponse("Missing user information", { status: 400 });
-    }
+    // if (!userInfo) {
+    //   return new NextResponse("Missing user information", { status: 400 });
+    // }
 
     const { displayName, email, role, photoURL } = userInfo;
 
     // Validate the data before creating a new user
-    if (!displayName || !email || !role || !photoURL) {
-      return new NextResponse("Invalid user data", { status: 400 });
-    }
+    // if (!displayName || !email || !role || !photoURL) {
+    //   return new NextResponse("Invalid user data", { status: 400 });
+    // }
 
     // Create a new user instance
-    const newUser = new Users({ displayName, email, photoURL, role });
+    await Users.create({displayName, email, photoURL, role},  )
 
     // Save the new user to the database
-    const savedUser = await newUser.save();
+    // const savedUser = await newUser.save();
 
-    return new NextResponse(JSON.stringify(savedUser), { status: 201 });
+    return NextResponse({message: "User stored"}, { status: 201 });
   } catch (error) {
     console.error("Database Error:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
