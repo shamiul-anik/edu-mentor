a6dacc2fd4e954a813c5ae3966230233a499c735
saidur-way <saiduronway@gmail.com>
2023-08-25 12:49:54 +0600

    swcMinify ok

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {}
 reactStrictMode: true,
 module.exports = {
+  swcMinify: false,
   // nextConfig,
   images: {
     domains: ["flowbite.s3.amazonaws.com", "www.google.com", "media.licdn.com","lh3.googleusercontent.com", "i.ibb.co"],
 
   },
 };
+
