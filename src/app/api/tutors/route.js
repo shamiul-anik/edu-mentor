beb491f2c9e326efd8d808f2e5a0d9f907672889
saidur-way <saiduronway@gmail.com>
2023-08-25 18:08:30 +0600

    cors inistall

--- src/app/api/tutors/route.js
+++ src/app/api/tutors/route.js
@@ -1,19 +1,65 @@
+// import DbConnect from "@/services/DbConnect";
+// import { NextResponse } from "next/server";
+
+// export const GET = async (request) =>{
+// // console.log(request.cookies, request.headers);
+// const db = await DbConnect();
+// const tutorsCollectiions = await db.collection('tutors');
+// const tutors = await tutorsCollectiions.find({}).toArray();
+
+// return NextResponse.json(tutors);
+// }
+
+// export const POST = async (request) => {
+//     const db = await DbConnect();
+//     const body = await request.json();
+//     const userCollections = await db.collection('users');
+//     userCollections.insertOne(body);
+    
+// }
+
+import Cors from 'cors';
 import DbConnect from "@/services/DbConnect";
 import { NextResponse } from "next/server";
 
-export const GET = async (request) =>{
-// console.log(request.cookies, request.headers);
-const db = await DbConnect();
-const tutorsCollectiions = await db.collection('tutors');
-const tutors = await tutorsCollectiions.find({}).toArray();
+// Initialize the cors middleware
+const cors = Cors({
+  origin: 'https://edu-mentor-demo.vercel.app', // Replace with your frontend domain
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+});
 
-return NextResponse.json(tutors);
+// Helper method to run the cors middleware
+// You can use this to apply cors to any API route
+async function applyCorsMiddleware(req, res) {
+  return new Promise((resolve, reject) => {
+    cors(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
 }
+
+export const GET = async (request) => {
+  // Apply the cors middleware to the request
+  await applyCorsMiddleware(request.req, request.res);
+
+  const db = await DbConnect();
+  const tutorsCollectiions = await db.collection('tutors');
+  const tutors = await tutorsCollectiions.find({}).toArray();
+
+  return NextResponse.json(tutors);
+};
 
 export const POST = async (request) => {
-    const db = await DbConnect();
-    const body = await request.json();
-    const userCollections = await db.collection('users');
-    userCollections.insertOne(body);
-    
-}
+  // Apply the cors middleware to the request
+  await applyCorsMiddleware(request.req, request.res);
+
+  const db = await DbConnect();
+  const body = await request.json();
+  const userCollections = await db.collection('users');
+  userCollections.insertOne(body);
+
+  return NextResponse.ok();
+};
