bd0963dc4546c31f11147796357f532bbec4fa2c
Asadullah850 <mhshamim850@gmail.com>
2023-08-28 20:28:48 +0600

    blog page add

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Blogs from './Blogs';
 import CommonBanner from '@/components/(shared)/CommonHeader/CommonBanner';
+import SectionTitle from '@/components/(shared)/SectionTitle/SectionTitle';
 
 
 const Blog = () => {
@@ -11,7 +12,7 @@ const Blog = () => {
         <div>
             {/* bg-teal-700 py-2 */}
             <CommonBanner bannerHeading="Blogs"></CommonBanner>
-            <h1 className='text-center text-xl my-2'>Blog</h1>
+            <SectionTitle heading="Blogs" subHeading="Your Thinking!"></SectionTitle>
             <Blogs></Blogs>
         </div>
     );
