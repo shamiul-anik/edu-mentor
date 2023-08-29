58bf7ae5031b11001a40de9e71f561fe35438cec
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-08-30 00:30:47 +0600

    update

--- src/app/api/students/route.js
+++ src/app/api/students/route.js
@@ -1,15 +1,13 @@
 import { NextResponse } from "next/server"
 import connect from "../../../utils/db"
-import {Students} from "../../../models/Students"
+import { Students } from "../../../models/Students"
 
-
-export const  GET = async (request) => {
- 
-    try {
-        await connect()
-        const students = await Students.find()
-        return new NextResponse(JSON.stringify(students), {status: 200})
-    } catch (error) {
-        return new NextResponse("Database Error!", {status: 500})
-    }
+export const GET = async (request) => {
+	try {
+		await connect()
+		const students = await Students.find()
+		return new NextResponse(JSON.stringify(students), { status: 200 })
+	} catch (error) {
+		return new NextResponse("Database Error!", { status: 500 })
+	}
 }
