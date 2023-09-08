780a205c8b0b378fd7dfb35c717d8c9976f7ef29
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-09-08 21:20:28 +0600

    update with issue

--- src/app/api/get-user/route.js
+++ src/app/api/get-user/route.js
@@ -8,16 +8,18 @@ export const GET = async (request, response) => {
     await connect();
     
     // Get the email parameter from the query string
-    const {email} = request.params.email;
+    const { searchParams } = new URL(request.url);
+    const email = searchParams.get("email");
     console.log(email);
 
     // Find the user by email
     const user = await Users.findOne({ email });
+    console.log(user);
     if (!user) {
       return new NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
-    return new NextResponse.json({ user }, { status: 200 });
+    return new NextResponse(JSON.stringify(user), { status: 200 });
   } catch (error) {
     console.error("Database Error:", error.message);
     return new NextResponse("Internal Server Error", { status: 500 });
