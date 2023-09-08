65e07ff827323d8f45ae6ec2be4e5e4fca300237
saidur-way <saiduronway@gmail.com>
2023-09-08 20:31:53 +0600

    change saidur many

--- src/app/popularTutors/[popularId]/page.tsx
+++ src/app/popularTutors/[popularId]/page.tsx
@@ -2,6 +2,7 @@
 import { Tutor } from '@/typeScript/tutorType';
 import Image from "next/image";
 import React from 'react';
+import BookingForm from "@/app/popularTutors/BookingForm"
 
 const singlePage: React.FC<{ params: { popularId: string } }> = async ({params: {popularId}}) => {
     
@@ -9,27 +10,42 @@ const singlePage: React.FC<{ params: { popularId: string } }> = async ({params: 
 		cache: 'no-cache'
 	})
 	const singleTutor = await res.json();
-    const { image_url, name, id, tuition_info, area_covered}= singleTutor || {};
+    const { image_url, name, id, tuition_info, area_covered, gender}= singleTutor || {};
     return (
+        <div className='flex '>
         <div className="m-10 w-1/2 bg-white ">
             <h3 className="text-3xl font-bold divide-x-2 divide-neutral-900 mb-4  ">Tutor Profile</h3>
 
 <div className="border border-gray-200 rounded-lg shadow">
 <a href="#" className="flex flex-col w-full items-center md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
     <Image width={190} height={256} className="object-cover w-full shadow-2xl md:h-auto " src={image_url} alt=""/>
-    <div className="grid grid-cols-3 justify-between gap-10 p-4 w-full leading-normal">
-        <div className="mb-2  ">
-            <p>Name:</p>
-            <p>ID#:</p>
-            <p>Gender:</p>
+    <div className=" w-full leading-normal">
+        {/* <div className="mb-2  ">
+            <span>Name:</span>
+            
+            <span>ID#:</span>
+            <span>Gender:</span>
             <p>Area Covered:</p>
             <p>Tutor Location:</p>
-             </div>
+             </div> */}
         <div className="mb-2 dark:text-white">
-           <p className="font-bold tracking-tight text-gray-900"> {name}</p>
-           <p> {id}</p>
-           <p> null</p>
-           <p> {
+            <p>
+            <span className='pr-4'>Name:</span>
+           <span className="font-bold tracking-tight text-gray-900"> {name}</span>
+            </p>
+           
+           <p>
+           <span  className='pr-8'>ID#:</span>
+           <span className="font-bold tracking-tight text-gray-900"> {id}</span>
+           </p>
+             <p>
+             <span className='pr-4'>Gender:</span>
+           <span className="font-bold tracking-tight text-gray-900">{gender}</span>
+             </p>
+             <hr className="mt-4 mb-4 border-1 border-slate-900" />
+           <p>
+           <span className="font-bold tracking-tight text-gray-900">Area Covered:</span>
+           <span> {
                 tuition_info?.preferred_areas_to_teach?.map((areas:any, index:any)=> <span
                  key={index}
                  className="pr-2"
@@ -37,8 +53,11 @@ const singlePage: React.FC<{ params: { popularId: string } }> = async ({params: 
                     
                     {areas }
                 </span>)
-            }</p>
-           <p> {
+            }</span>
+           </p>
+            <p>
+            <span className="font-bold tracking-tight text-gray-900">Tutor Location:</span>
+           <span> {
                 area_covered?.map((area:any, index:any)=> <span
                 key={index}
                 className="pr-2"
@@ -46,14 +65,19 @@ const singlePage: React.FC<{ params: { popularId: string } }> = async ({params: 
                    
                    {area}
                </span>)
-            }</p>
+            }</span>
 
+            </p>
         </div>
     </div>
 </a>
 
 </div>
 
+        </div>
+        <div className= "m-10 w-1/2 border  border-gray-200">
+        <BookingForm/>
+        </div>
         </div>
     );
 };
