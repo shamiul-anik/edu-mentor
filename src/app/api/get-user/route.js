373e6ecbd9583108ce3a486ad8cc6bd160cac38e
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-09-13 21:55:50 +0600

    fetch issues fixed

--- src/app/api/get-user/route.js
+++ src/app/api/get-user/route.js
@@ -6,20 +6,23 @@ export const GET = async (request, response) => {
   try {
     // Connect to the MongoDB database
     await connect();
-    
+
     // Get the email parameter from the query string
     const { searchParams } = new URL(request.url);
     const email = searchParams.get("email");
-    console.log(email);
+    const query = { email };
+
 
     // Find the user by email
-    const user = await Users.findOne({ email });
-    console.log(user);
-    if (!user) {
+    if (query.email !== null) {
+      const user = await Users.findOne(query);
+      console.log(user);
+      return new NextResponse(JSON.stringify(user), { status: 200 });
+    }
+    else if(query.email === null) {
       return new NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
-    return new NextResponse(JSON.stringify(user), { status: 200 });
   } catch (error) {
     console.error("Database Error:", error.message);
     return new NextResponse("Internal Server Error", { status: 500 });
