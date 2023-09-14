ef5917b8451c628b763c4ba5f761ebb15cc6fcad
shamiul <anik.savar.bd@gmail.com>
2023-09-14 22:40:56 +0600

    error fixing by returning to previous version of the get-user and get-bookings route

--- src/app/api/get-user/route.js
+++ src/app/api/get-user/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import connect from "../../../utils/db";
 import { Users } from "../../../models/Users";
 
-export const GET = async (request) => {
+export const GET = async (request, response) => {
   try {
     // Connect to the MongoDB database
     await connect();
@@ -12,19 +12,19 @@ export const GET = async (request) => {
     const email = searchParams.get("email");
     const query = { email };
 
-
     // Find the user by email
     if (query.email !== null) {
       const user = await Users.findOne(query);
       console.log(user);
       return new NextResponse(JSON.stringify(user), { status: 200 });
+    } else if (query.email === null) {
+      return new NextResponse.json(
+        { error: "User not found" },
+        { status: 404 }
+      );
     }
-    else if(query.email === null) {
-      return new NextResponse.json({ error: "User not found" }, { status: 404 });
-    }
-
   } catch (error) {
     console.error("Database Error:", error.message);
-    return NextResponse("Internal Server Error", { status: 500 });
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 };
