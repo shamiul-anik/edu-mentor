691dbd1b4df459522e9148d2755a964086462938
saidur-way <saiduronway@gmail.com>
2023-09-15 17:01:45 +0600

    admin dashboard update

--- src/app/dashboard/admin/manage-tutor-request/page.jsx
+++ src/app/dashboard/admin/manage-tutor-request/page.jsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { GrValidate } from "react-icons/gr";
 import { LuShieldClose } from "react-icons/lu";
 import { VscFeedback } from "react-icons/vsc";
+import { useEffect, useState } from "react";
+import useAuth from "@/hooks/useAuth";
 // import FeedbackModal from "./FeedbackModal";
 // import SingleClass from "./SingleClass";
 // import { useQuery } from "@tanstack/react-query";
@@ -14,6 +16,28 @@ import { VscFeedback } from "react-icons/vsc";
 // import useAuth from "../../../../hooks/useAuth";
 
 const ManageStudents = () => {
+
+  const [allRequest, setAllRequest] = useState([]);
+  const { user, loading, userRole } = useAuth();
+	
+	useEffect(() => {
+		const fetchAllTutorRequest = async () => {
+		  try {
+			const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tutor-request`,
+			{
+			  cache: 'no-cache'
+			});
+			const data = await res.json();
+			setAllRequest(data);
+		  } catch (error) {
+			console.error('Error fetching mentor data:', error);
+		   
+		  }
+		};
+	
+		fetchAllTutorRequest();
+	  }, []);
+    console.log(allRequest);
 
   // Sample Data
   const admin_feedback = "Good work!"
@@ -174,79 +198,86 @@ const ManageStudents = () => {
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td className="px-2 py-2 whitespace-nowrap text-center border-r-2">
-                  1
-                </td>
-                {/* <td className="px-2 py-2 text-center border-r-2">
-                  <div className="avatar flex items-center justify-center ">
-                    <div className="w-24 rounded-xl">
-                      <Image src="" alt={`Image of Tutor`} />
+              {
+                allRequest?.map((request, index) =>(
+                  <tr
+                  key={request._id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="px-2 py-2 whitespace-nowrap text-center border-r-2">
+                    {index + 1}
+                  </td>
+                  {/* <td className="px-2 py-2 text-center border-r-2">
+                    <div className="avatar flex items-center justify-center ">
+                      <div className="w-24 rounded-xl">
+                        <Image src="" alt={`Image of Tutor`} />
+                      </div>
                     </div>
-                  </div>
-                </td> */}
-                <td className="px-2 py-2 whitespace-nowrap border-r-2">
-                  {/* {class_name} */}
-                </td>
-                <td className="px-2 py-2 whitespace-nowrap text-center border-r-2">
-                  {/* {instructor_name} */}
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  {/* {instructor_email} */}
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  {/* {available_seats} */}
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  {/* ${class_price} */}
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  
-                </td>
-                <td className="px-2 py-2 text-center border-r-2">
-                  
-                </td>
-                <td className="px-2 py-2 text-center uppercase border-r-2">
-                  {/* {class_status} */}
-                </td>
-                <td className="px-2 py-2 border-r-2">
-                  {/* {admin_feedback} */}
-                </td>
-                <td className="px-2 py-2 text-center">
-                  <button type="button" className="flex w-44 mx-auto justify-center items-center text-white bg-gradient-to-br from-green-500 to-green-600 transition-all hover:duration-300 hover:from-green-600 hover:to-green-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700" disabled={isVerified} >
-                    <GrValidate className='gr-icon w-4 h-4 mr-2' />
-                    Approve
-                  </button>
-                  <button type="button" className="flex w-44 mx-auto mt-2 justify-center items-center text-white bg-gradient-to-br from-red-500 to-red-600 transition-all hover:duration-300 hover:from-red-600 hover:to-red-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700" disabled={isVerified}>
-                    <LuShieldClose className='gr-icon w-4 h-4 mr-2' />
-                    Deny
-                  </button>
-                  {
-                    admin_feedback ?
-                      <button type="button" className="flex w-44 mx-auto mt-2 justify-center items-center text-white bg-gradient-to-br from-teal-500 to-teal-600 transition-all hover:duration-300 hover:from-teal-600 hover:to-teal-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-teal-200 dark:focus:ring-teal-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700">
-                        <VscFeedback className='gr-icon w-4 h-4 mr-2' />
-                        Update Feedback
-                      </button>
-                      :
-                      <button type="button" className="flex w-44 mx-auto mt-2 justify-center items-center text-white bg-gradient-to-br from-blue-500 to-blue-600 transition-all hover:duration-300 hover:from-blue-600 hover:to-blue-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700">
-                        <VscFeedback className='gr-icon w-4 h-4 mr-2' />
-                        Send Feedback
-                      </button>
-                  }
-                </td>
-              </tr>
+                  </td> */}
+                  <td className="px-2 py-2 whitespace-nowrap border-r-2">
+                    {request?.name}
+                  </td>
+                  <td className="px-2 py-2 whitespace-nowrap text-center border-r-2">
+                  {request?.email}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.title}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.mobileNumber}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.salary}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.tuitionType}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.medium}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.subject}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.classname}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.district}
+                  </td>
+                  <td className="px-2 py-2 text-center border-r-2">
+                    {request?.area}
+                  </td>
+                  <td className="px-2 py-2 text-center uppercase border-r-2">
+                    {(request?.isVerified == true) ? <span className= "text-green-700">true</span> : <span className="text-red-700"> false</span>}
+                  </td>
+                  <td className="px-2 py-2 border-r-2">
+                   continue work
+                  </td>
+                  <td className="px-2 py-2 text-center">
+                    <button type="button" className="flex w-44 mx-auto justify-center items-center text-white bg-gradient-to-br from-green-500 to-green-600 transition-all hover:duration-300 hover:from-green-600 hover:to-green-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700" disabled={request?.isVerified} >
+                      <GrValidate className='gr-icon w-4 h-4 mr-2' />
+                      Approve
+                    </button>
+                    <button type="button" className="flex w-44 mx-auto mt-2 justify-center items-center text-white bg-gradient-to-br from-red-500 to-red-600 transition-all hover:duration-300 hover:from-red-600 hover:to-red-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700" disabled={isVerified}>
+                      <LuShieldClose className='gr-icon w-4 h-4 mr-2' />
+                      Deny
+                    </button>
+                    {
+                      admin_feedback ?
+                        <button type="button" className="flex w-44 mx-auto mt-2 justify-center items-center text-white bg-gradient-to-br from-teal-500 to-teal-600 transition-all hover:duration-300 hover:from-teal-600 hover:to-teal-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-teal-200 dark:focus:ring-teal-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700">
+                          <VscFeedback className='gr-icon w-4 h-4 mr-2' />
+                          Update Feedback
+                        </button>
+                        :
+                        <button type="button" className="flex w-44 mx-auto mt-2 justify-center items-center text-white bg-gradient-to-br from-blue-500 to-blue-600 transition-all hover:duration-300 hover:from-blue-600 hover:to-blue-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700">
+                          <VscFeedback className='gr-icon w-4 h-4 mr-2' />
+                          Send Feedback
+                        </button>
+                    }
+                  </td>
+                </tr>
+                ))
+              }
+
             </tbody>
           </table>
         </div>
---
+++ src/utils/dashboard/adminBtn.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Swal from "sweetalert2";
+const adminBtn = (user, value) => {
+    console.log(user._id, value)
+    console.log(user._id, value)
+    const fetchAdminBtn = async () => {
+		  try {
+			const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/dashboard/admin?id=${user?._id}&controlAdminBtn=${value}`, {
+        method: "PATCH",
+      },
+			{
+			  cache: 'no-store'
+			});
+      if (res.status === 200) {
+        // Successful response, handle data accordingly
+        // setAllUsers(data);
+        Swal.fire({
+          position: 'top-center',
+          icon: 'success',
+          title: "User Action successfully",
+          showConfirmButton: false,
+          timer: 1500
+        })
+        console.log("User admin action successfully")
+        
+      }
+			const data = await res.json();
+      console.log(data)
+
+			
+		  } catch (error) {
+			console.error('Error fetching All userdata:', error);
+		   
+		  }
+		};
+	
+		fetchAdminBtn();
+};
+
+export default adminBtn;
