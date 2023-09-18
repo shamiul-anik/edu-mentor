48f38072923b995d2ecaf5babb33d11f9d4bb74d
saidur-way <saiduronway@gmail.com>
2023-09-18 21:59:51 +0600

    change tutor collection to user collection and loade tutor

--- src/app/api/tutors/route.js
+++ src/app/api/tutors/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connect from "@/utils/db";
-import { Tutors } from "@/models/Tutors";
+import { Users } from "@/models/Users";
 
 
 export const GET = async (request) => {
@@ -8,7 +8,7 @@ export const GET = async (request) => {
     await connect();
     const {searchParams} = new URL(request.url);
     const gender = (searchParams.get("gender"))
-    const premium = (searchParams.get("premium"))
+    const isVerified = (searchParams.get("isVerified"))
     // console.log(gender,premium)
 
     
@@ -20,25 +20,27 @@ export const GET = async (request) => {
 
 
     // Create a filter object based on the provided query parameters
-    const filter = {};
+    const filter = {
+      role: "tutor"
+    };
 
     if (gender === 'male' || gender === "female") {
       filter.gender = gender;
     }
 
-    if (premium === "true") {
-      filter.premium = true;
+    if (isVerified === "true") {
+      filter.isVerified = true;
     }
     console.log(filter)
     // Check if both gender and premium are not provided in the query parameters
-    if (!gender && !premium) {
+    if (!gender && !isVerified) {
       // If both are not provided, find all tutors without any filters
-      const tutors = await Tutors.find();
+      const tutors = await Users.find(filter);
       return new NextResponse(JSON.stringify(tutors), { status: 200 });
     }
 
     // Use Mongoose to find tutors based on the filter
-    const tutors = await Tutors.find(filter);
+    const tutors = await Users.find(filter);
 
     return new NextResponse(JSON.stringify(tutors), { status: 200 });
   } catch (error) {
