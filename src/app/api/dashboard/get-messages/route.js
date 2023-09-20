69cd7702281c09f069eef92a9b05800fc89d9d59
saidur-way <saiduronway@gmail.com>
2023-09-20 13:00:46 +0600

    student message api

---
+++ src/app/api/dashboard/get-messages/route.js
@@ -0,0 +1,37 @@
+import connect from "@/utils/db.js";
+import { NextResponse } from "next/server";
+import { Student_Message } from "../../../../models/Student_Message"
+
+export const  GET = async (request) => {
+    try {
+        await connect()
+        const {searchParams} = new URL(request.url);
+        const tutor_email = searchParams.get('tutor_email');
+        const student_email = searchParams.get('student_email');
+
+        const query = {
+
+        }
+        
+        if(tutor_email){
+            query.tutor_email = tutor_email;
+        }
+        if(student_email){
+            query.student_email = student_email;
+        }
+
+        console.log('get-messages route', query)
+        if(query ){
+            console.log('get-messages route block', query)
+            const messages = await Student_Message.find(query)
+            return new NextResponse(JSON.stringify(messages), {status: 200})
+        }
+
+        const allMessages = await Student_Message.find();
+        return new NextResponse(JSON.stringify(allMessages), { status: 200 });
+        
+        
+    } catch (error) {
+        return new NextResponse.json("Database Error", error, {status: 500});
+    }
+}
