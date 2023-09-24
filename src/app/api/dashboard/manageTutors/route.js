cf8a6e190a9ebdc43736390bbc50f17d8c50ffcb
shamiul <anik.savar.bd@gmail.com>
2023-09-24 19:57:17 +0600

    Code clean up and feedback modal functionality and content updated

--- src/app/api/dashboard/manageTutors/route.js
+++ src/app/api/dashboard/manageTutors/route.js
@@ -1,47 +1,41 @@
 import connect from "@/utils/db.js";
 import Tutor_request from "../../../../models/Tutor_request";
 import { NextResponse } from "next/server";
+
 export const PATCH = async (request) => {
-  try {
-    await connect();
-    const { searchParams } = new URL(request.url);
-    const id = searchParams.get("id");
-    const controlAdminBtn = searchParams.get("controlAdminBtn");
-    const feedback = searchParams.get("feedback");
-    // const role = searchParams.get("role");
-    // console.log("manage tutors route",id, controlAdminBtn);
+	try {
+		await connect();
+		const { searchParams } = new URL(request.url);
+		const controlAdminBtn = searchParams.get("controlAdminBtn");
+		const id = searchParams.get("id");
+		const feedback = searchParams.get("feedback");
 
-    const query = {
-      id
-    };
-    const query2 = {
+		const query = { id };
+		const feedback_query = { };
+		
+		if (feedback) {
+			feedback_query.admin_feedback = feedback;
+		}
 
-    }
-    // all admin btn action
-    // console.log("query",query);
-    console.log("20", feedback)
-    if(feedback){
-      query2.admin_feedback = feedback;
-    }
-    console.log("20 ", query2)
-    const hasValidQuery = Object.keys(query2).length > 0;
-    if(hasValidQuery){
-      console.log("manageTutors", query)
-      const tutorRequest = await Tutor_request.findByIdAndUpdate( id, query2 );
-  return NextResponse.json({tutorRequest }, { status: 200 });
-    }
-    
-    if (controlAdminBtn == "approve") {
-        const tutorRequest = await Tutor_request.findByIdAndUpdate( id,
-            { isVerified: true });
-      return NextResponse.json({tutorRequest }, { status: 200 });
-    }else if (controlAdminBtn == "deny") {
-        const tutorRequest = await Tutor_request.findByIdAndUpdate( id,
-            { isVerified: false });
-      return NextResponse.json({ tutorRequest }, { status: 200 });
-    }
-  } catch (error) {
-    console.error("Database Error:", error.message);
-    return new NextResponse.error("Internal Server Error", { status: 500 });
-  }
+		const hasValidQuery = Object.keys(feedback_query).length > 0;
+		if (hasValidQuery) {
+			const tutorRequest = await Tutor_request.findByIdAndUpdate(id, feedback_query);
+			return NextResponse.json({ tutorRequest }, { status: 200 });
+		}
+
+		if (controlAdminBtn == "approve") {
+			const tutorRequest = await Tutor_request.findByIdAndUpdate(id, { isVerified: true });
+			return NextResponse.json({ tutorRequest }, { status: 200 });
+		} 
+
+		else if (controlAdminBtn == "deny") {
+			const tutorRequest = await Tutor_request.findByIdAndUpdate(id, { isVerified: false });
+			return NextResponse.json({ tutorRequest }, { status: 200 });
+		}
+
+	} 
+	catch (error) {
+		console.error("Database Error:", error.message);
+		return new NextResponse.error("Internal Server Error", { status: 500 });
+	}
 };
