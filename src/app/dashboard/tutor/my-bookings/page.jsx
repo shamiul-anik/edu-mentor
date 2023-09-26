41ad67bad74c54530be78a0b24714332d945d45e
shamiul <anik.savar.bd@gmail.com>
2023-09-26 20:20:53 +0600

    Tutor dashboard contents updated, issue fixing and code clean up

--- src/app/dashboard/tutor/my-bookings/page.jsx
+++ src/app/dashboard/tutor/my-bookings/page.jsx
@@ -2,14 +2,14 @@
 import useAuth from '@/hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState, useTransition } from 'react';
+import { Fade } from 'react-awesome-reveal';
 import toast from 'react-hot-toast';
 import { GrValidate } from 'react-icons/gr';
 import { LuShieldClose } from 'react-icons/lu';
-import Swal from 'sweetalert2';
 
 const MyBookings = () => {
 	const { user } = useAuth();
-	const [tutorBooking, setTutorBooking] = useState([]);
+	const [tutorBookings, setTutorBookings] = useState([]);
 	const [isPending, startTransition] = useTransition();
 	const router = useRouter();
 	useEffect(() => {
@@ -22,9 +22,10 @@ const MyBookings = () => {
 					}
 				);
 				const data = await res.json();
-				console.log(data);
-				setTutorBooking(data);
-			} catch (error) {
+				// console.log(data);
+				setTutorBookings(data);
+			}
+			catch (error) {
 				console.error("Error fetch Student Booking data:", error);
 			}
 		};
@@ -33,45 +34,26 @@ const MyBookings = () => {
 	}, [user?.email]);
 	// console.log(tutorBooking);
 
-	const handleAdminBtn= (id, value) => {
-    // adminBtn(user, value);
-    
+	const handleAdminBtn = (id, value) => {
 
-  // console.log(user._id, value)
-
-    const fetchAdminBtn = async () => {
-		  try {
-			const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/dashboard/manageBooking?id=${id}&controlAdminBtn=${value}`, {
-        method: "PATCH",
-		cache: "no-store"
-      });
-      if (res.status === 200) {
-        // Successful response, handle data accordingly
-        // setAllUsers(data);
-        // Swal.fire({
-        //   position: 'center',
-        //   icon: 'success',
-        //   title: "User Action successfully",
-        //   showConfirmButton: false,
-        //   timer: 1500
-        // })
-        // console.log("User admin action successfully")
-		toast.success("User Action successfully")
-        
-      }
-	  startTransition(()=>{
-		console.log({router})
-		router.refresh();
-	  })
-			const data = await res.json();
-
-      // console.log(data)
-
-			
-		  } catch (error) {
-			console.error('Error fetching adminAction btn:', error);
-		   
-		  }
+		const fetchAdminBtn = async () => {
+			try {
+				const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/dashboard/manageBooking?id=${id}&controlAdminBtn=${value}`, {
+					method: "PATCH",
+					cache: "no-store"
+				});
+				if (res.status === 200) {
+					toast.success("User action successful!")
+				}
+				startTransition(() => {
+					// console.log({ router })
+					router.refresh();
+				})
+				// const data = await res.json();
+			}
+			catch (error) {
+				console.error('Error in fetching: ', error);
+			}
 		};
 		fetchAdminBtn();
 	};
@@ -79,14 +61,13 @@ const MyBookings = () => {
 		<>
 			<header>
 				<h1 className="text-5xl text-teal-700 font-bold text-center mt-4 lg:mt-8">
-					{/* <Fade duration={200} triggerOnce={true} cascade>Add a Tuition</Fade> */}
-					<span>My Bookings</span>
+					<Fade duration={200} triggerOnce={true} cascade>My Bookings</Fade>
 				</h1>
 			</header>
 			<section className="max-w-7xl mx-auto mt-4 lg:mt-8 p-4 md:px-0">
-				{/* <div>
-				<h1 className="text-3xl font-bold text-center mb-6">Total Enrolled Classes: {myEnrolledClasses?.length}</h1>
-				</div> */}
+				<div>
+					<h1 className="text-3xl font-bold text-center mb-6">Total Bookings: {tutorBookings?.length}</h1>
+				</div>
 
 				<div className="relative overflow-x-auto">
 					<table className="border-2 border-slate-200 w-full text-sm text-left text-gray-1000 dark:text-gray-400">
@@ -114,7 +95,7 @@ const MyBookings = () => {
 									scope="col"
 									className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2"
 								>
-									Phone <br /> Number
+									Mobile <br /> Number
 								</th>
 								<th
 									scope="col"
@@ -150,7 +131,7 @@ const MyBookings = () => {
 									scope="col"
 									className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2"
 								>
-									Verification <br /> Status
+									Acceptance <br /> Status
 								</th>
 								<th
 									scope="col"
@@ -161,60 +142,54 @@ const MyBookings = () => {
 							</tr>
 						</thead>
 						<tbody>
-							{tutorBooking?.map((tbook, index) => (
+							{tutorBookings?.map((booking, index) => (
 								<tr
-									key={tbook?._id}
+									key={booking?._id}
 									className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
 								>
 									<td className="px-2 py-2 whitespace-nowrap text-center border-r-2">
 										{index + 1}
 									</td>
 									<td className="px-2 py-2 border-r-2">
-										{tbook?.student_name}
+										{booking?.student_name}
 									</td>
 									<td className="px-2 py-2 border-r-2">
-										{tbook?.student_email}
+										{booking?.student_email}
 									</td>
 									<td className="px-2 py-2 border-r-2">
-										{tbook?.student_mobile_number}
+										{booking?.student_mobile_number}
 									</td>
-									<td className="px-2 py-2 border-r-2">{tbook?.subject}</td>
+									<td className="px-2 py-2 border-r-2">{booking?.subject}</td>
 									<td className="px-2 py-2 border-r-2">
-										{tbook?.student_class}
+										{booking?.class_name}
 									</td>
 									<td className="px-2 py-2 border-r-2">
-										{tbook?.student_location}
+										{booking?.student_location}
 									</td>
 									<td className="px-2 py-2 text-center border-r-2">
-										{tbook?.salary}
+										{booking?.salary}
 									</td>
 									<td className="px-2 py-2 text-center border-r-2">
-										{tbook?.available_days}
+										{booking?.available_days}
 									</td>
 									<td className="px-2 py-2 text-center border-r-2">
-										{tbook?.isAccepted == null
-											? "Pending"
-											: tbook?.isAccepted == false
-											? "Rejected"
-											: tbook?.isAccepted == true
-											? "Accepted"
-											: ""}
+										{booking?.isAccepted === true ? "Accepted" : booking?.isAccepted === false ? "Rejected" : "Pending"}
 									</td>
 									<td className="px-2 py-2 text-center border-r-2">
 										<button
-											onClick={() => handleAdminBtn(tbook?._id, "approve")}
+											onClick={() => handleAdminBtn(booking?._id, "approve")}
 											type="button"
 											className="flex w-40 mx-auto justify-center items-center text-white bg-gradient-to-br from-green-500 to-green-600 transition-all hover:duration-300 hover:from-green-600 hover:to-green-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700"
-											disabled={tbook?.isAccepted}
+											disabled={booking?.isAccepted}
 										>
 											<GrValidate className="gr-icon w-4 h-4 mr-2" />
 											Approve
 										</button>
 										<button
-											onClick={() => handleAdminBtn(tbook?._id, "deny")}
+											onClick={() => handleAdminBtn(booking?._id, "deny")}
 											type="button"
 											className="flex w-40 mx-auto mt-2 justify-center items-center text-white bg-gradient-to-br from-red-500 to-red-600 transition-all hover:duration-300 hover:from-red-600 hover:to-red-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800 font-normal rounded-md text-md px-3 py-2 text-center disabled:from-slate-600 disabled:to-slate-700"
-											disabled={tbook?.isAccepted ? false : true}
+											disabled={booking?.isAccepted ? false : true}
 										>
 											<LuShieldClose className="gr-icon w-4 h-4 mr-2" />
 											Deny
--- src/app/dashboard/tutor/my-tuitions/page.jsx
+++ src/app/dashboard/tutor/my-tuitions/page.jsx
@@ -1,9 +1,10 @@
 "use client"
 import useAuth from '@/hooks/useAuth';
 import React, { useEffect, useState } from 'react';
+import { Fade } from 'react-awesome-reveal';
 
 const MyTuitions = () => {
-  const { user } = useAuth();
+	const { user } = useAuth();
 	const [myTuitions, setMyTuitions] = useState([]);
 	useEffect(() => {
 		const fetchMyTuitions = async () => {
@@ -15,7 +16,7 @@ const MyTuitions = () => {
 					}
 				);
 				const data = await res.json();
-				console.log(data);
+				// console.log(data);
 				setMyTuitions(data);
 			} catch (error) {
 				console.error("Error fetchStudentBooking data:", error);
@@ -24,83 +25,85 @@ const MyTuitions = () => {
 
 		fetchMyTuitions();
 	}, [user?.email]);
-	console.log(myTuitions)
-  return (
+	// console.log(myTuitions)
+	return (
 
-     <>
-      <header>
-        <h1 className="text-5xl text-teal-700 font-bold text-center mt-4 lg:mt-8">
-          {/* <Fade duration={200} triggerOnce={true} cascade>Add a Tuition</Fade> */}
-          <span>My Tuitions</span>
-        </h1>
-      </header>
-    <section className="max-w-7xl mx-auto mt-4 lg:mt-8 p-4 md:px-0">
+		<>
+			<header>
+				<h1 className="text-5xl text-teal-700 font-bold text-center mt-4 lg:mt-8">
+					<Fade duration={200} triggerOnce={true} cascade>My Tuitions</Fade>
+				</h1>
+			</header>
+			<section className="max-w-7xl mx-auto mt-4 lg:mt-8 p-4 md:px-0">
 
-      {/* <div>
-        <h1 className="text-3xl font-bold text-center mb-6">Total Enrolled Classes: {myEnrolledClasses?.length}</h1>
-      </div> */}
+				<div>
+					<h1 className="text-3xl font-bold text-center mb-6">Total Registered Tuitions: {myTuitions?.length}</h1>
+				</div>
 
-      <div className="relative overflow-x-auto">
-        <table className="border-2 border-slate-200 w-full text-sm text-left text-gray-1000 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
-                #
-              </th>
-              <th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
-                Subject <br /> Name
-              </th>
-              <th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
-                Class <br /> Name
-              </th>
-              <th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
-                Location
-              </th>
-              <th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
-                Salary Range<br /> (Per Month)
-              </th>
-              <th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
-                Available <br /> Days
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              myTuitions?.map((tuition, index)=> (
-                <tr
-                key={tuition?._id}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td className="px-2 py-2 whitespace-nowrap text-center border-r-2">
-                  {index + 1}
-                </td>
-                <td className="px-2 py-2 border-r-2">
-                  {tuition?.subject}
-                </td>
-                <td className="px-2 py-2 border-r-2">
-                  {tuition?.class_name}
-                </td>
-                <td className="px-2 py-2 border-r-2">
-                  {tuition?.service_location}
-                </td>
-                <td className="px-2 py-2 border-r-2">
-                  {tuition?.salary}
-                </td>
-                <td className="px-2 py-2 border-r-2">
-                  {tuition?.available_days
-}
-                </td>
-              </tr>
-              ))
-            }
+				<div className="relative overflow-x-auto">
+					<table className="border-2 border-slate-200 w-full text-sm text-left text-gray-1000 dark:text-gray-400">
+						<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+							<tr>
+								<th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
+									#
+								</th>
+								<th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
+									Subject <br /> Name
+								</th>
+								<th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
+									Class <br /> Name
+								</th>
+								<th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
+									Location
+								</th>
+								<th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
+									Salary<br /> (Per Month)
+								</th>
+								<th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
+									Available <br /> Days
+								</th>
+								<th scope="col" className="text-center bg-gray-100 px-3 py-4 border-b-2 border-r-2">
+									Acceptance <br /> Status
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{
+								myTuitions?.map((tuition, index) => (
+									<tr
+										key={tuition?._id}
+										className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+										<td className="px-2 py-2 whitespace-nowrap text-center border-r-2">
+											{index + 1}
+										</td>
+										<td className="px-2 py-2 border-r-2">
+											{tuition?.subject}
+										</td>
+										<td className="px-2 py-2 border-r-2 text-center">
+											{tuition?.class_name}
+										</td>
+										<td className="px-2 py-2 border-r-2">
+											{tuition?.service_location}
+										</td>
+										<td className="px-2 py-2 border-r-2 text-center">
+											{tuition?.salary}
+										</td>
+										<td className="px-2 py-2 border-r-2">
+											{tuition?.available_days}
+										</td>
+										<td className="px-2 py-2 border-r-2 text-center">
+											{tuition?.isVerified === true ? "Accepted" : tuition?.isVerified === false ? "Rejected" : "Pending"}
+										</td>
+									</tr>
+								))
+							}
+						</tbody>
+					</table>
+				</div>
 
-
-          </tbody>
-        </table>
-      </div>
-
-    </section>
-    </>
-  );
+			</section>
+		</>
+	);
 };
 
 export default MyTuitions;
