b64e04051a2d125f3ba60bb22cdcef39e0a9a488
shamiul <anik.savar.bd@gmail.com>
2023-09-27 05:08:17 +0600

    Email payment notification to the student and to the tutor added upon a payment attempt by a student.

---
+++ src/utils/sslPaymentEmailNotification/sendPaymentNotificationEmail.js
@@ -0,0 +1,27 @@
+const sendPaymentNotificationEmail = async (payment) => {
+
+	try {
+		const response = await fetch(
+			`${process.env.NEXT_PUBLIC_API_URL}/api/ssl-payment/payment-email-notification`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(payment),
+			}
+		);
+
+		if (response.ok) {
+			return "Email sent successfully!";
+		}
+		else {
+			return "Email sending failed!";
+		}
+	} 
+	catch (error) {
+		return `Error sending email: ${error}`;
+	}
+};
+
+export default sendPaymentNotificationEmail;
